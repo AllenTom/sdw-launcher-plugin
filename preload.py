bc9a03e5b341ae6c00bcb84fa19938e5c14f5288
AllenTom <qweasdzxczse2@gmail.com>
2023-07-12 21:40:34 +0800

    update

--- preload.py
+++ preload.py
@@ -1,3 +1,4 @@
 def preload(parser):
     parser.add_argument("--launcher-session", type=str, help="launcher session", default="global")
     parser.add_argument("--launcher-callback", type=str, help="launcher session", default="http://localhost:6745")
+    parser.add_argument("--launcher-enable", action="store_true", help="enable launcher", default=False)
--- scripts/launcher_script.py
+++ scripts/launcher_script.py
@@ -1,6 +1,5 @@
 import os
 
-import aiohttp
 import gradio as gr
 import requests
 from fastapi import FastAPI
@@ -8,16 +7,20 @@ from fastapi import FastAPI
 from modules import script_callbacks, shared
 
 baseCallbackUrl = "http://localhost:6745"
+enable_plugin = False
 
 
 def app_started(_: gr.Blocks, app: FastAPI):
     global baseCallbackUrl
     baseCallbackUrl = shared.cmd_opts.launcher_callback
-    print("app started")
-    r = requests.post(baseCallbackUrl + "/sdw/appstarted", json={
-        "pid": os.getpid(),
-        "session": shared.cmd_opts.launcher_session,
-    })
+    global enable_plugin
+    enable_plugin = shared.cmd_opts.launcher_enable
+    print("launcher plugin started")
+    if enable_plugin:
+        r = requests.post(baseCallbackUrl + "/sdw/appstarted", json={
+            "pid": os.getpid(),
+            "session": shared.cmd_opts.launcher_session,
+        })
 
 
 script_callbacks.on_app_started(app_started)
